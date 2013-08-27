e1f16943681f5114e1857423b66e946d366d8f0b
Bartek Antosik <antosik.bartek@gmail.com>
2013-08-27 16:14:55 +0200

    Added basic tool for simulating result of a single test and gathering statistics

--- gcgt_tests.py
+++ gcgt_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 import graph_constr_group_testing as gt_init
+from graph_constr_group_testing import problem_json_io as problem_io
 from graph_constr_group_testing import graph_constr_group_testing as gt
 
 import networkx as nx
@@ -26,9 +27,25 @@ class Test(unittest.TestCase):
         
 
     def test_read_problem_from_file(self):
-        g = gt.read_problem_from_file("test_data/test1.json")
+        g = problem_io.read_problem_from_file("test_data/test1.json")
         self.assertEquals(g.faulty_set, set(["1","3","5"]), "Faulty set read from problem description %s" % (g.faulty_set,))
         self.assertTrue(graph_equal(g.problem_graph.graph, self.problem1_graph), "should get right graph")
         self.assertEquals(g.problem_graph.source, "START", "Source should be START but is %s" % (g.problem_graph.source))
-        self.assertEquals(g.problem_graph.sink, "START", "Source should be START but is %s" % (g.problem_graph.sink))
+        self.assertEquals(g.problem_graph.sink, "END", "Source should be START but is %s" % (g.problem_graph.sink))
+        
+    def test_path_tester(self):
+        g = problem_io.read_problem_from_file("test_data/test1.json")
+        path_tester = gt.PathTester(g.faulty_set)
+        p1 = ["2","4","6"]
+        p2 = ["1","4","6"]
+        p3 = ["2","3","6"]
+        result = path_tester.test_path(p1)
+        self.assertFalse(result, "For %s path test1 should give negative result" % (p1,))
+        result = path_tester.test_path(p2)
+        self.assertTrue(result, "For %s path test1 should give positive result" % (p2,))
+        result = path_tester.test_path(p3)
+        self.assertTrue(result, "For %s path test1 should give positive result" % (p3,))
+        self.assertEquals(path_tester.get_positive_queries(), 2, "Should be two positive queries got %d" % (path_tester.get_positive_queries(),))
+        self.assertEquals(path_tester.get_negative_queries(), 1, "Should be one negative queries got %d" % (path_tester.get_negative_queries(),))
+        self.assertEquals(path_tester.get_all_queries(), 3, "Should be 3 queries in total got %d" % (path_tester.get_all_queries(),))
         
--- graph_constr_group_testing/graph_constr_group_testing.py
+++ graph_constr_group_testing/graph_constr_group_testing.py
@@ -1,30 +1,25 @@
-import collections
-import json
-import networkx as nx
-
-
-Problem = collections.namedtuple("Problem", ["problem_graph", "faulty_set", "description"])
-ProblemGraph = collections.namedtuple("ProblemGraph", ["graph", "source", "sink"])
-
-def read_problem_from_file(file):
-    with open(file, "r") as f:
-        data = json.load(f)
-        
-        faulty_set = set(data['faulty_nodes'])
-        problem_graph = create_problem_graph(data['graph'])
-        if "description" in data:
-            description = "".join(data['description'])
-        else:
-            description = ""
-        
-        problem = Problem(problem_graph, faulty_set, description)
-        
-    return problem
-
-def create_problem_graph(data_source):
-    graph = nx.DiGraph(data_source['edges'])
-    source = data_source['source_node']
-    sink = data_source['sink_node']
-    problem_graph = ProblemGraph(graph, source, sink)
-    return problem_graph
+class PathTester(object):
+    def __init__(self, faulty_set):
+        self.faulty_set = faulty_set
+        self.positive_queries = 0
+        self.negative_queries = 0
     
+    def test_path(self, path):
+        result = any((x in self.faulty_set) for x in path)
+        if result:
+            self.positive_queries = self.positive_queries + 1
+        else:
+            self.negative_queries = self.negative_queries + 1
+        return result 
+    
+    def get_all_queries(self):
+        return self.positive_queries + self.negative_queries
+    
+    def get_negative_queries(self):
+        return self.negative_queries
+    
+    def get_positive_queries(self):
+        return self.positive_queries
+    
+        
+            
