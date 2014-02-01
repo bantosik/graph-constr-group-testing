0041ac36dfb66c3358bfff0b75e869083b4f1f45
Bartek Antosik <antosik.bartek@gmail.com>
2014-02-01 13:01:17 +0100

    Extracted TestStatistics from PathTester class

--- gcgt_tests.py
+++ gcgt_tests.py
@@ -1,10 +1,9 @@
 import unittest
 
-import graph_constr_group_testing as gt_init
+import networkx as nx
+
 from graph_constr_group_testing import problem_json_io as problem_io
 from graph_constr_group_testing import graph_constr_group_testing as gt
-
-import networkx as nx
 
 def graph_equal(g1, g2):
     return set(g1.edges_iter()) == set(g2.edges_iter())
@@ -24,18 +23,19 @@ class Test(unittest.TestCase):
         self.problem1_graph.add_edge("4", "6")
         self.problem1_graph.add_edge("5", "END")
         self.problem1_graph.add_edge("6", "END")
-        
+
 
     def test_read_problem_from_file(self):
         g = problem_io.read_problem_from_file("test_data/test1.json")
-        self.assertEquals(g.faulty_set, set(["1","3","5"]), "Faulty set read from problem description %s" % (g.faulty_set,))
+        self.assertEquals(g.faulty_set, {"1","3","5"}, "Faulty set read from problem description %s" % (g.faulty_set,))
         self.assertTrue(graph_equal(g.problem_graph.graph, self.problem1_graph), "should get right graph")
         self.assertEquals(g.problem_graph.source, "START", "Source should be START but is %s" % (g.problem_graph.source))
         self.assertEquals(g.problem_graph.sink, "END", "Source should be START but is %s" % (g.problem_graph.sink))
-        
+
     def test_path_tester(self):
         g = problem_io.read_problem_from_file("test_data/test1.json")
-        path_tester = gt.PathTester(g.faulty_set)
+        stats = gt.TestStatistics()
+        path_tester = gt.PathTester(g.faulty_set, stats)
         p1 = ["2","4","6"]
         p2 = ["1","4","6"]
         p3 = ["2","3","6"]
@@ -45,20 +45,22 @@ class Test(unittest.TestCase):
         self.assertTrue(result, "For %s path test1 should give positive result" % (p2,))
         result = path_tester.test_path(p3)
         self.assertTrue(result, "For %s path test1 should give positive result" % (p3,))
-        self.assertEquals(path_tester.get_positive_queries(), 2, "Should be two positive queries got %d" % (path_tester.get_positive_queries(),))
-        self.assertEquals(path_tester.get_negative_queries(), 1, "Should be one negative queries got %d" % (path_tester.get_negative_queries(),))
-        self.assertEquals(path_tester.get_all_queries(), 3, "Should be 3 queries in total got %d" % (path_tester.get_all_queries(),))
-        
-        
+        self.assertEquals(stats.get_positive_queries(), 2, "Should be two positive queries got %d" % (stats.get_positive_queries(),))
+        self.assertEquals(stats.get_negative_queries(), 1, "Should be one negative queries got %d" % (stats.get_negative_queries(),))
+        self.assertEquals(stats.get_all_queries(), 3, "Should be 3 queries in total got %d" % (stats.get_all_queries(),))
+
+
     def test_brute_force_solver(self):
         g = problem_io.read_problem_from_file("test_data/test1.json")
         solver = gt.BruteForceSolver(g)
         solution = solver.solve()
         stats = solution[1]
-        faulty_set = solution[0] 
-        self.assertEquals(faulty_set, g.faulty_set, "Should find all nodes from faulty set %s, got only %s" % 
+        faulty_set = solution[0]
+        self.assertEquals(faulty_set, g.faulty_set, "Should find all nodes from faulty set %s, got only %s" %
                           (g.faulty_set, faulty_set))
         self.assertEquals(stats.get_all_queries(), 8, "Should have 8 queries in total got %d" % (stats.get_all_queries(),))
         self.assertEquals(stats.get_positive_queries(), 7, "Should have 7 positive queries in total got %d" % (stats.get_positive_queries(),))
         self.assertEquals(stats.get_negative_queries(), 1, "Should have 1 positive queries in total got %d" % (stats.get_negative_queries(),))
-        
+
+if __name__ == "__main__":
+    pass
--- graph_constr_group_testing/graph_constr_group_testing.py
+++ graph_constr_group_testing/graph_constr_group_testing.py
@@ -1,32 +1,42 @@
-import collections 
+"""Module contains basic solver and statistic gathering utility classes"""
+
+import collections
+
+Problem = collections.namedtuple("Problem", ["problem_graph", "faulty_set", "description"])
+ProblemGraph = collections.namedtuple("ProblemGraph", ["graph", "source", "sink"])
 
 class BruteForceSolver(object):
     def __init__(self, problem_description):
+        """
+        :type problem_description: Problem
+        """
         self.problem_description = problem_description
-        self.tester = PathTester(problem_description.faulty_set)
         self.graph = self.problem_description.problem_graph.graph
         self.source = self.problem_description.problem_graph.source
         self.sink = self.problem_description.problem_graph.sink
-                
+
+        self.statistics = TestStatistics()
         self.analysis_state = BruteForceSolver.SimpleStateAnalyser(self.graph.nodes_iter())
-        
+        self.tester = PathTester(problem_description.faulty_set, self.statistics)
+
+
     def solve(self):
         for path in generate_paths(self.graph, self.source, self.sink):
             result = self.tester.test_path(path)
-            self.tester.end_run()
+            self.statistics.end_run()
             self.analysis_state.put_result(result, path)
-        return self.analysis_state.get_positive_elements(), self.tester    
-    
+        return self.analysis_state.get_positive_elements(), self.statistics
+
     class SimpleStateAnalyser(object):
         def __init__(self, nodes_iterator):
             self.nodes = collections.defaultdict(int)
             for i in nodes_iterator:
                 self.nodes[i] = False
-            
+
         def put_result(self, result, elements):
             for e in elements:
                 self.nodes[e] = result
-                
+
         def get_positive_elements(self):
             return set([e for e, state in self.nodes.iteritems() if state])
                 
@@ -39,26 +49,37 @@ def generate_paths(graph, start_point, end_point):
                 yield [begin] + p
         
 class PathTester(object):
-    def __init__(self, faulty_set):
+    def __init__(self, faulty_set, stats):
         self.faulty_set = faulty_set
-        self.positive_queries = 0
-        self.negative_queries = 0
-        self.number_of_runs = 0
-    
+        self.stats = stats
+
     def test_path(self, path):
         result = any((x in self.faulty_set) for x in path)
         if result:
-            self.positive_queries = self.positive_queries + 1
+            self.stats.inc_positive_query()
         else:
-            self.negative_queries = self.negative_queries + 1
-        return result 
+            self.stats.inc_negative_query()
+        return result
+
+
+class TestStatistics(object):
+    def __init__(self):
+        self.number_of_runs = 0
+        self.positive_queries = 0
+        self.negative_queries = 0
     
     def end_run(self):
         self.number_of_runs = self.number_of_runs + 1
         
     def get_all_queries(self):
         return self.positive_queries + self.negative_queries
-    
+
+    def inc_positive_query(self):
+        self.positive_queries += 1
+
+    def inc_negative_query(self):
+        self.negative_queries += 1
+
     def get_negative_queries(self):
         return self.negative_queries
     
