32b2ca250c972c9fc427f149540fe0cbecd9ac3c
Bartek Antosik <antosik.bartek@gmail.com>
2014-02-24 22:25:10 +0100

    Made interface of test statistics more dynamic.

--- graph_constr_group_testing/base_types.py
+++ graph_constr_group_testing/base_types.py
@@ -50,45 +50,18 @@ class TestStatistics(object):
     Maintains various statistics related with the single run of group testing algorithm
     """
     def __init__(self):
-        self.number_of_runs = 0
-        self.positive_queries = 0
-        self.negative_queries = 0
+        self.variable_dict = {}
 
-    def end_run(self):
-        """
-        called when a batch of paths is inserted to testing system :class:``
-        """
-        self.number_of_runs = self.number_of_runs + 1
+    def set_var(self, var, value):
+        self.variable_dict[var] = value
 
-    def get_all_queries(self):
-        """
-        check number of all queries (paths)
-        """
-        return self.positive_queries + self.negative_queries
+    def get_var(self, var):
+        return self.variable_dict[var]
 
-    def inc_positive_query(self):
-        """
-        called when a query returns positive result
-        """
-        self.positive_queries += 1
+    def inc_var(self, var):
+        self.variable_dict[var] = self.variable_dict.get(var, 0) + 1
 
-    def inc_negative_query(self):
-        """
-        called when a query returns negative result
-        """
-        self.negative_queries += 1
 
-    def get_negative_queries(self):
-        return self.negative_queries
-
-    def get_positive_queries(self):
-        return self.positive_queries
-
-    def get_number_of_runs(self):
-        return self.number_of_runs
-
-    def set_state(self, state):
-        self.state = state
 
 
 class GCGTSolver(object):
